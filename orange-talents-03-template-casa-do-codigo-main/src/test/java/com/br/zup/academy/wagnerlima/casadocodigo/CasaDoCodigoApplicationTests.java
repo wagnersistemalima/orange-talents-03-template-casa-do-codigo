bdca0729ada593c03f90dacb795b8ec8e1d81bab
wagnersistemalima <wagner.sistemalima@gmail.com>
2021-03-19 18:27:04 -0300

    setup do projeto

---
+++ orange-talents-03-template-casa-do-codigo-main/src/test/java/com/br/zup/academy/wagnerlima/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.zup.academy.wagnerlima.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
